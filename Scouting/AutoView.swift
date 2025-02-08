1806e17831e399197c224258c167fdfaabc28fce
Adam E <Adam.m.elsafty@gmail.com>
2025-02-08 14:28:52 -0500

    Even more nicing nice nice one bro

--- Scouting/AutoView.swift
+++ Scouting/AutoView.swift
@@ -6,7 +6,7 @@ struct AutoView: View {
         Form {
             Section(header: Text("Autonomous Phase")) {
                 
-
+                ToggleInput(label: "Robot Left Starting Line", isOn: $autoData.robotLeftStartingLine)
                 StepperInput(label: "Coral Level 1", value: $autoData.coral1, range: 0...10)
                 StepperInput(label: "Coral Level 2", value: $autoData.coral2, range: 0...10)
                 StepperInput(label: "Coral Level 3", value: $autoData.coral3, range: 0...10)
@@ -14,7 +14,7 @@ struct AutoView: View {
 
                 StepperInput(label: "Processor Score", value: $autoData.processorScore, range: 0...10)
                 StepperInput(label: "Net Algae Score", value: $autoData.netAlgae, range: 0...10)
-                ToggleInput(label: "Robot Left Starting Line", isOn: $autoData.robotLeftStartingLine)
+                
             }
         }
         .navigationTitle("Auto Phase")
--- Scouting/TeleopView.swift
+++ Scouting/TeleopView.swift
@@ -7,7 +7,7 @@ struct TeleopView: View {
         Form {
             Section(header: Text("Teleop Phase")) {
                 
-
+                ToggleInput(label: "Knocked Off Algae", isOn: $teleopData.knockedOffAlgae)
                 StepperInput(label: "Coral Level 1", value: $teleopData.coral1, range: 0...10)
                 StepperInput(label: "Coral Level 2", value: $teleopData.coral2, range: 0...10)
                 StepperInput(label: "Coral Level 3", value: $teleopData.coral3, range: 0...10)
@@ -15,7 +15,7 @@ struct TeleopView: View {
 
                 StepperInput(label: "Processor Score", value: $teleopData.processorScore, range: 0...10)
                 StepperInput(label: "Net Algae Score", value: $teleopData.netAlgae, range: 0...10)
-                ToggleInput(label: "Knocked Off Algae", isOn: $teleopData.knockedOffAlgae)
+                
             }
         }
         .navigationTitle("Teleop Phase")
