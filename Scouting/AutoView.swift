991254b28a868d0c81cc7620ed5e61c9d0af948f
Drake <drakemcgillivray@cmail.carleton.ca>
2025-02-12 17:08:15 -0500

    More counters needed

--- Scouting/AutoView.swift
+++ Scouting/AutoView.swift
@@ -7,13 +7,13 @@ struct AutoView: View {
             Section(header: Text("Autonomous Phase")) {
                 
                 ToggleInput(label: "Robot Left Starting Line", isOn: $autoData.robotLeftStartingLine)
-                StepperInput(label: "Coral Level 1", value: $autoData.coral1, range: 0...10)
-                StepperInput(label: "Coral Level 2", value: $autoData.coral2, range: 0...10)
-                StepperInput(label: "Coral Level 3", value: $autoData.coral3, range: 0...10)
-                StepperInput(label: "Coral Level 4", value: $autoData.coral4, range: 0...10)
+                StepperInput(label: "Coral Level 1", value: $autoData.coral1, range: 0...100)
+                StepperInput(label: "Coral Level 2", value: $autoData.coral2, range: 0...100)
+                StepperInput(label: "Coral Level 3", value: $autoData.coral3, range: 0...100)
+                StepperInput(label: "Coral Level 4", value: $autoData.coral4, range: 0...100)
 
-                StepperInput(label: "Processor Score", value: $autoData.processorScore, range: 0...10)
-                StepperInput(label: "Net Algae Score", value: $autoData.netAlgae, range: 0...10)
+                StepperInput(label: "Processor Score", value: $autoData.processorScore, range: 0...100)
+                StepperInput(label: "Net Algae Score", value: $autoData.netAlgae, range: 0...100)
                 
             }
         }
--- Scouting/TeleopView.swift
+++ Scouting/TeleopView.swift
@@ -8,13 +8,13 @@ struct TeleopView: View {
             Section(header: Text("Teleop Phase")) {
                 
                 ToggleInput(label: "Knocked Off Algae", isOn: $teleopData.knockedOffAlgae)
-                StepperInput(label: "Coral Level 1", value: $teleopData.coral1, range: 0...10)
-                StepperInput(label: "Coral Level 2", value: $teleopData.coral2, range: 0...10)
-                StepperInput(label: "Coral Level 3", value: $teleopData.coral3, range: 0...10)
-                StepperInput(label: "Coral Level 4", value: $teleopData.coral4, range: 0...10)
+                StepperInput(label: "Coral Level 1", value: $teleopData.coral1, range: 0...100)
+                StepperInput(label: "Coral Level 2", value: $teleopData.coral2, range: 0...100)
+                StepperInput(label: "Coral Level 3", value: $teleopData.coral3, range: 0...100)
+                StepperInput(label: "Coral Level 4", value: $teleopData.coral4, range: 0...100)
 
-                StepperInput(label: "Processor Score", value: $teleopData.processorScore, range: 0...10)
-                StepperInput(label: "Net Algae Score", value: $teleopData.netAlgae, range: 0...10)
+                StepperInput(label: "Processor Score", value: $teleopData.processorScore, range: 0...100)
+                StepperInput(label: "Net Algae Score", value: $teleopData.netAlgae, range: 0...100)
                 
             }
         }
